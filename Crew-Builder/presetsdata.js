a8a7297a18fdfbdb65487fe7ed511e6b424cb433
john-neely <104113514+john-neely@users.noreply.github.com>
2026-03-20 02:18:33 -0400

    Add files via upload

---
+++ Crew-Builder/presetsdata.js
@@ -0,0 +1,44 @@
+/* -------------------------
+PRESET BUILDS
+------------------------- */
+
+const presets = {
+
+    "BB Standard 4 Gunner": [
+        "BO",
+        "Scout",
+        "Main Gunner",
+        "Main Gunner",
+        "Main Gunner",
+        "Main Gunner",
+        "Engineer",
+        "Engineer",
+        "Engineer",
+        "Engineer"
+    ],
+
+    "BB Repair Heavy": [
+        "BO",
+        "Scout",
+        "Main Gunner",
+        "Main Gunner",
+        "Main Gunner",
+        "Main Gunner",
+        "Repair",
+        "Repair",
+        "Repair",
+        "Engineer"
+    ],
+
+    "CV Standard": [
+        "BO",
+        "Main Gunner",
+        "Main Gunner",
+        "Fighter",
+        "Fighter",
+        "Fighter",
+        "Fighter",
+        "Seaman"
+    ]
+
+};
